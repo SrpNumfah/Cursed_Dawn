18235fc779d0320e52b15c4cc34ac09054167f10
SrpNumfah <jellyboofah2000@gmail.com>
2024-04-28 11:04:47 +0700

    fix spawn position_ enemy

--- Assets/Script/Enemy/EnemyRandom.cs
+++ Assets/Script/Enemy/EnemyRandom.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 public class EnemyRandom : MonoBehaviour
 {
     public GameObject enemyPrefabs;
-
-
+    public Transform[] spawnPoint;
+    private GameObject[] _spawnEnemy;
+    public int enemyCount;
 
     private void Start()
     {
-        SpawnEnemy(5);
+        this.enemyCount = Random.Range(5, 10);
+        SpawnEnemy(enemyCount);
+       
     }
 
-    public void SpawnEnemy(int spawnCount)
+    public void SpawnEnemy(int enemyCount)
     {
+        _spawnEnemy = new GameObject[enemyCount];
         
-        for (int i = 0; i < spawnCount; i++)
-        {
-            Vector3 spawnPosition = new Vector3(Random.Range(-50, 55), Random.Range(-33, -35));
-            Instantiate(enemyPrefabs, spawnPosition, Quaternion.identity);
 
+        for (int i = 0; i < enemyCount; i++)
+        {
+            int randomSpawmIndex = Random.Range(0, spawnPoint.Length);
+            Vector3 randomPosition = spawnPoint[randomSpawmIndex].position;
+            _spawnEnemy[i] = Instantiate(enemyPrefabs, randomPosition, Quaternion.identity);
             continue;
         }
-      
     }
 
    
